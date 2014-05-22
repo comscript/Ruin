57e014295f6186198b53a8ed4bec651fdf93ac01
Colin Graber <colin.graber@gmail.com>
2014-05-22 17:57:24 -0400

    Tweaked values a bit

--- src/entity/grenade.rb
+++ src/entity/grenade.rb
@@ -1,7 +1,7 @@
 class Grenade < CPEntity
 
   def create(x,y)
-    super(10, 150, x, y)
+    super(10, CP::moment_for_circle(10,0,8,CP::Vec2.new(0,0)), x, y)
     self.sprite="grenade.png" 
   end
 
